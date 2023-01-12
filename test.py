bad3da35dda613add64590f013ef5bd2c8d88748
Kristucism <121264013+Kristucism@users.noreply.github.com>
2023-01-12 23:07:09 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+from datetime import datetime
+
+print(datetime.now().year)
+
+city="Riga"
+country="Latvia"
+
+api_key = "60aa068482d6ddc251ae5f53570ac5fb"
+
+weather_url = requests.get(f'http://api.openweather.org/data/2.5/weather?appid={api_key}&q={Riga},{country}&units=imperial')
+
+weather_data = weather_url.json()
+
+temp = round(weather_data['main']['temp'])
+humidity = weather_data['main']['humidity']
+wind_speed =nweather_data ['wind'][_'speed'_]
